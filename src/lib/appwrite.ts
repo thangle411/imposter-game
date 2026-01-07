d29a0eb062714920a02f1f71629d28559e055d63
Thang Le <thangleee228@gmail.com>
2026-01-07 01:43:08 -0800

    remove console.log in appwrite

--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -1,7 +1,6 @@
 import { Client, Account } from "appwrite";
 
 const { VITE_APPWRITE_ENDPOINT, VITE_APPWRITE_PROJECT_ID } = import.meta.env;
-console.log(VITE_APPWRITE_ENDPOINT, VITE_APPWRITE_PROJECT_ID)
 
 const client = new Client()
     .setEndpoint(VITE_APPWRITE_ENDPOINT)
